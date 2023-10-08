14f26e0f49ee3dbe0e7d243a1ab4b31c601339ea
Dean Glueckler <glueckler@gmail.com>
2023-10-08 11:31:48 -0700

    added echo

--- main.go
+++ main.go
@@ -1,25 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"os"
+
+	"github.com/labstack/echo/v4"
 )
- 
+
 func main() {
-	port := os.Getenv("PORT")
- 
-	if port == "" {
-		port = "8080"
-	}
- 
-	http.HandleFunc("/", HelloHandler)
- 
-	log.Println("Listening on port", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-}
- 
-func HelloHandler(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprintf(w, "Hello from Koyeb")
+    e := echo.New()
+    e.GET("/", func(c echo.Context) error {
+        return c.String(http.StatusOK, "Hello, World!")
+    })
+    e.Logger.Fatal(e.Start(":1323"))
 }
